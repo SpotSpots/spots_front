775e889bd755f839fe33870a27ffb3899b3cc8b1
alsrudrl1220 <alsrudrl1220@naver.com>
2023-11-24 20:23:02 +0900

    feat: 검색 결과 데모 화면 추가
    - main.dart and result page added

---
+++ lib/ResultPage.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  // 나중에 기능 추가하면서 삭제
+  int _myCounter = 0;
+  int _yourCounter = 0;
+  void _refreshGoalCounter() {
+    setState(() {
+      _myCounter = 0;
+      _yourCounter = 0;
+    });
+  }
+  //
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffE9E9E9),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xffE9E9E9),
+        title: Text('Reslts', style: TextStyle(fontWeight: FontWeight.bold),),
+        leading: IconButton(
+            icon: Icon(Icons.navigate_before, size: 28),
+            onPressed: () {
+              print('menu button is clicked');
+            }
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.more_horiz),
+              onPressed: () {
+                print('clicked');
+              }
+          ),
+        ],
+      ),
+
+      body: Container(
+        child: ListView(
+          children: <Widget> [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                // 1. 검색창
+                SizedBox(height: 10,),
+                ElevatedButton(style : ElevatedButton.styleFrom(
+                  minimumSize: Size.zero,
+                  padding: EdgeInsets.fromLTRB(10, 15, 350, 15),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ), onPressed : _refreshGoalCounter, child: const Icon(Icons.search)),
+
+                // 2. 정렬, 필터링 버튼 // 작동 되도록 수정
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(style : ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ), onPressed : _refreshGoalCounter, child: const Icon(Icons.tune)),
+                    ElevatedButton(style : ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.fromLTRB(15, 7, 15, 7),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ), onPressed : _refreshGoalCounter, child: const Text('Sort V')),
+                    ElevatedButton(style : ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.fromLTRB(15, 7, 15, 7),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ), onPressed : _refreshGoalCounter, child: const Text('Open Now')),
+                    ElevatedButton(style : ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.fromLTRB(15, 7, 15, 7),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ), onPressed : _refreshGoalCounter, child: const Text('Price V')),
+                    ElevatedButton(style : ElevatedButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.fromLTRB(15, 7, 15, 7),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ), onPressed : _refreshGoalCounter, child: const Text('Wi-Fi')),
+                  ],
+                ),
+
+                // 3. 카페 이미지 + 정보 //stack으로 별점, 찜 추가
+                SizedBox(height: 30,),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    children: [
+                      const Image(
+                          image: AssetImage(
+                              'assets/cafe1.png'
+                          ),
+                          width: 380
+                      ),
+                      Container(
+                        width: 380,
+                        height: 110,
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Column(
+                                  children: [
+                                    Text('KRISP Fresh Living', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.location_on),
+                                        Text('2.4 mi // Irvine'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 110,
+                                ),
+                                ElevatedButton(style : ElevatedButton.styleFrom(minimumSize: Size(60, 30)), onPressed: _refreshGoalCounter, child: Text('Check In')),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(width: 2),
+                                Icon(Icons.wifi),
+                                Text('Wi-fi'),
+                                SizedBox(width: 5),
+                                Icon(Icons.videocam),
+                                Text('Outlets'),
+                                SizedBox(width: 5),
+                                Icon(Icons.volume_up),
+                                Text('Moderate Noise'),
+                                SizedBox(width: 8),
+                                Text('+ 5 More', style: TextStyle(color: Colors.grey),), // 누르면 나머지 보여주기?
+                                SizedBox(width: 2),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // 4. 3과 동일하게...
+                SizedBox(height: 10,),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    children: [
+                      const Image(
+                          image: AssetImage(
+                              'assets/cafe1.png'
+                          ),
+                          width: 380
+                      ),
+                      Container(
+                        width: 380,
+                        height: 110,
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                const Column(
+                                  children: [
+                                    Text('KRISP Fresh Living', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Icon(Icons.location_on),
+                                        Text('2.4 mi // Irvine'),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 110,
+                                ),
+                                ElevatedButton(
+                                    style : ElevatedButton.styleFrom(minimumSize: Size(60, 30)),
+                                    onPressed: _refreshGoalCounter,
+                                    child: Text('Check In')),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(width: 2),
+                                Icon(Icons.wifi),
+                                Text('Wi-fi'),
+                                SizedBox(width: 5),
+                                Icon(Icons.videocam),
+                                Text('Outlets'),
+                                SizedBox(width: 5),
+                                Icon(Icons.volume_up),
+                                Text('Moderate Noise'),
+                                SizedBox(width: 8),
+                                Text('+ 5 More', style: TextStyle(color: Colors.grey),), // 누르면 나머지 보여주기?
+                                SizedBox(width: 2),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
