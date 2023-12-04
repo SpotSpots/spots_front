0a077e71326c8089ef771e32eaaeea9ec3b5cd45
gwaksital <80732503+gwaksital@users.noreply.github.com>
2023-12-04 20:56:11 +0900

    ing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotsfront/TabControlPage.dart';
 import 'firebase_options.dart';
-import 'TabControlPage.dart';
 import 'LogoPage.dart';
+import 'package:spotsfront/fab_with_icons.dart';
+import 'package:spotsfront/fab_bottom_app_bar.dart';
+import 'package:spotsfront/layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,6 +40,80 @@ class MyApp extends StatelessWidget {
             return const LogoPage();
           }
         },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  String _lastSelected = 'TAB: 0';
+
+  void _selectedTab(int index) {
+    setState(() {
+      _lastSelected = 'TAB: $index';
+    });
+  }
+
+  void _selectedFab(int index) {
+    setState(() {
+      _lastSelected = 'FAB: $index';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          _lastSelected,
+          style: TextStyle(fontSize: 32.0),
+        ),
+      ),
+      bottomNavigationBar: FABBottomAppBar(
+        centerItemText: 'Explore',
+        color: Colors.blue,
+        selectedColor: Colors.red,
+        notchedShape: CircularNotchedRectangle(),
+        onTabSelected: _selectedTab,
+        items: [
+          FABBottomAppBarItem(iconData: Icons.home, text: 'Home'),
+          FABBottomAppBarItem(iconData: Icons.favorite, text: 'Saved'),
+          FABBottomAppBarItem(iconData: Icons.chat_bubble, text: 'Activity'),
+          FABBottomAppBarItem(iconData: Icons.person, text: 'Profile'),
+        ], backgroundColor: Colors.white,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: _buildFab(
+          context), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildFab(BuildContext context) {
+    final icons = [ Icons.school, Icons.local_cafe, Icons.more ];
+    return AnchoredOverlay(
+      showOverlay: true,
+      overlayBuilder: (context, offset) {
+        return CenterAbout(
+          position: Offset(offset.dx, offset.dy - icons.length * 35.0),
+          child: FabWithIcons(
+            icons: icons,
+            onIconTapped: _selectedFab,
+          ),
+        );
+      },
+      child: FloatingActionButton(
+        onPressed: () { },
+        tooltip: 'Increment',
+        child: Icon(Icons.explore),
+        elevation: 2.0,
+        shape: CircleBorder(),
       ),
     );
   }
