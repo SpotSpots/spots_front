3c1d0bedb5a8acf45841e41e7effc38ad04744b4
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2023-12-10 18:17:35 +0900

    [fix] collection 삭제 + cafe spot & study spot으로 변경
    - 둘 다 horizontal scroll로 설정
    - firebase와 연동 필요
    - scroll시 color 변경 안되도록 고정함

--- lib/SavedPage.dart
+++ lib/SavedPage.dart
@@ -15,6 +15,7 @@ class _SavedPageState extends State<SavedPage> {
       backgroundColor: Color(0xffE9E9E9),
       appBar: AppBar(
         centerTitle: true,
+        backgroundColor: Colors.transparent, // 고정 색상 지정
         // backgroundColor: Color(0xffE9E9E9),
         title: Text('Saved', style: TextStyle(fontWeight: FontWeight.bold),),
         leading: IconButton(
@@ -36,10 +37,10 @@ class _SavedPageState extends State<SavedPage> {
           child: Container(
             //padding: EdgeInsets.all(8.0),
             alignment: Alignment.center,
-            child: Text(
-              'drag and drop to groups',
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-            ),
+            // child: Text(
+            //   'drag and drop to groups',
+            //   style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+            // ),
           ),
         ),
       ),
@@ -47,15 +48,14 @@ class _SavedPageState extends State<SavedPage> {
 
       body: Column(
         children: [
-
-          // 1. Collection horizontal view
+          // 1. Cafe Spots
           const SizedBox(height: 20,),
           const Align(
             alignment: Alignment.centerLeft,
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
               child: Text(
-                'Collections',
+                'Cafe Spots',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
@@ -67,94 +67,6 @@ class _SavedPageState extends State<SavedPage> {
               scrollDirection: Axis.horizontal, // 가로로 스크롤 해서 collections 확인
               itemCount: 4,
               padding: EdgeInsets.symmetric(horizontal: 16),
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return Container(
-                    width: 120,
-                    height: 150,
-                    margin: EdgeInsets.only(right: 8),
-                    child: Column(
-                      children: [
-                        Card(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(16.0),
-                            child: Container(
-                              width: 120,
-                              height: 120,
-                              child: const Center(
-                                child: Text(
-                                  '+',
-                                  style: TextStyle(
-                                    fontSize: 40,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  // Regular treatment for the other containers
-                  return Container(
-                    width: 120,
-                    height: 150,
-                    margin: EdgeInsets.only(right: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Card(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(16.0),
-                            child: Container(
-                              width: 120,
-                              height: 120,
-                              child: Image.asset(
-                                'assets/cafe1.png',
-                                fit: BoxFit.fitHeight,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const Text(
-                          'Fev cafes in OC',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        const Text(
-                          'saved',
-                          style: TextStyle(fontSize: 12, color: Colors.grey, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              },
-            ),
-            ),
-
-          // 2. Saved Spots (46) view
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Saved Spots ()',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          SizedBox(height: 10,),
-          Flexible(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                //crossAxisSpacing: 6.0, // 아이템 간의 가로 간격
-                mainAxisSpacing: 8.0, // 아이템 간의 세로 간격
-              ),
-              itemCount: 6,
               itemBuilder: (context, index) {
                 return Container(
                   width: 150,
@@ -209,10 +121,6 @@ class _SavedPageState extends State<SavedPage> {
                         'Fev cafes in OC',
                         style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                      const Text(
-                        'Costa Mesa, CA',
-                        style: TextStyle(fontSize: 12, color: Colors.blue, fontWeight: FontWeight.bold),
-                      ),
                     ],
                   ),
                 );
@@ -220,7 +128,85 @@ class _SavedPageState extends State<SavedPage> {
             ),
           ),
 
-
+          // 2. Study Spots
+          const SizedBox(height: 20,),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'Study Spots',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal, // 가로로 스크롤 해서 collections 확인
+              itemCount: 4,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 150,
+                  height: 210,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          Card(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16.0),
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2.0,
+                                ),
+                              ),
+                              width: 150,
+                              height: 150,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16.0),
+                                child: Image.asset(
+                                  'assets/cafe1.png',
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 10,
+                            right: 10,
+                            child: Container(
+                              width: 32,
+                              height: 32,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.favorite, color: Colors.pink),
+                                onPressed: () {
+                                  print('heart button clicked');
+                                },
+                                iconSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        'Fev cafes in OC',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
