144bcbf763fd62e63cea471d32ab48cd7a32ac1b
planepaper <eddysonkr@gmail.com>
2023-12-11 21:47:34 +0900

    feat: ExploreButton 검색 창으로 연결되도록 구현
    - 페이지 연결 및 아이콘 이미지도 바꾸었습니다

--- lib/ExploreButton.dart
+++ lib/ExploreButton.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'fab_with_icons.dart';
 import 'layout.dart';
+import 'ResultPage.dart';
+import 'CafeService.dart';
 
 class ExploreButton extends StatelessWidget {
   ExploreButton({Key? key}) : super(key: key);
 
-  final icons = [Icons.school, Icons.local_cafe, Icons.cast_for_education, Icons.book, Icons.bed];
+  final icons = [Icons.electric_bolt, Icons.computer, Icons.person, Icons.people, Icons.apartment];
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,42 @@ class ExploreButton extends StatelessWidget {
           child: FabWithIcons(
             icons: icons,
             onIconTapped: (int index) {
-              print("Icon tapped: ${icons[index]}");
+              //(승표) SUPER HARD CODING
+              if(index == 0)
+              {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ResultPage(searchKeyword: '207', cafeQuery: CafeService().getCafesBySearchKeyword('207'))));
+              }
+              else if (index == 1)
+              {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ResultPage(searchKeyword: '208', cafeQuery: CafeService().getCafesBySearchKeyword('208'))));
+              }
+              else if(index == 2)
+              {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ResultPage(searchKeyword: '308', cafeQuery: CafeService().getCafesBySearchKeyword('308'))));
+              }
+              else if(index == 3)
+              {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ResultPage(searchKeyword: '309', cafeQuery: CafeService().getCafesBySearchKeyword('309'))));
+              }
+              else if(index == 4)
+              {
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ResultPage(searchKeyword: '310', cafeQuery: CafeService().getCafesBySearchKeyword('310'))));
+              }
             },
           ),
         );
