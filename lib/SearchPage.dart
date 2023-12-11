9cb8919b324c745c233c3dc64fd804ec4aca4960
planepaper <eddysonkr@gmail.com>
2023-12-11 23:17:16 +0900

    Revert "SearchPage 배경색 변경"
    This reverts commit 091a8262628d0205330431515e5471adcc36e73e.

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spotsfront/RecentSearches.dart';
 import 'ResultPage.dart';
 import 'CafeService.dart';
 
@@ -10,13 +12,15 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String searchKeyword = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffD5EAF7),
+      backgroundColor: Color(0xffE9E9E9),
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: const Color(0xffD5EAF7),
+          backgroundColor: Color(0xffE9E9E9),
           title: Text('Search', style: TextStyle(fontWeight: FontWeight.bold),),
           leading: IconButton(
             icon: Icon(Icons.navigate_before, size: 28),
@@ -33,75 +37,130 @@ class _SearchPageState extends State<SearchPage> {
           ],
         ),
       body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                minimumSize: Size.zero,
-                padding: const EdgeInsets.fromLTRB(10, 15, 0, 15),
-                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+        child: Expanded(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                width: MediaQuery.of(context).size.width,
+                height: 54,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 20,
+                      offset: Offset(0, 11), // changes position of shadow
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        autofocus: true,
+                        style: TextStyle(fontSize:15),
+                        decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            border: InputBorder. none,
+                            hintText: 'Search cafe, library, study centers...',
+                            hintStyle: TextStyle(color: Colors.grey)
+                        ),
+                        onChanged: (value){
+                          setState(() {
+                            searchKeyword = value;
+                          });
+                        },
+                        onSubmitted: (value) {
+                          context.read<RecentSearches>().addSearchKeyword(searchKeyword);
+                          Navigator.push(context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    ResultPage(searchKeyword: searchKeyword, cafeQuery: CafeService().getCafesBySearchKeyword(searchKeyword))));
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {
-              },
-              child: const Row(
-                children: [
-                  Icon(Icons.search),
-                  SizedBox(width: 10),
-                  Text(
-                    'Search cafe, library, study centers...',
-                    style: TextStyle(color: Colors.grey),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 20,
+                  shadowColor: Colors.grey.withOpacity(0.5),
+                  backgroundColor: Colors.white,
+                  minimumSize: Size.zero,
+                  padding: const EdgeInsets.fromLTRB(10, 15, 0, 15),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ResultPage(searchKeyword: 'Current Location', cafeQuery: CafeService().getAllCafes()),));
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.location_on),
+                    SizedBox(width: 10),
+                    Text(
+                      'Current Location',
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                minimumSize: Size.zero,
-                padding: const EdgeInsets.fromLTRB(10, 15, 0, 15),
-                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            ResultPage(searchKeyword: 'Current Location', cafeQuery: CafeService().getAllCafes()),));
-              },
-              child: const Row(
-                children: [
-                  Icon(Icons.location_on),
-                  SizedBox(width: 10),
-                  Text(
-                    'Current Location',
-                    style: TextStyle(color: Colors.grey),
+                  color: Colors.white,
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.all(15),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.white,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Recent Searches",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15,15,0,0),
+                        child: const Text(
+                          "Recent Searches",
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Expanded(
+                        child:  ListView.builder(
+                            itemCount: context.watch<RecentSearches>().searchList.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                  leading: const Icon(Icons.search),
+                                  title: Text(context.watch<RecentSearches>().searchList[index]),
+                                  onTap: (){
+                                    Navigator.push(context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ResultPage(searchKeyword: searchKeyword,
+                                                    cafeQuery: CafeService().getCafesBySearchKeyword(
+                                                        context.read<RecentSearches>().searchList[index]))));
+                                  },
+                              );
+                            },
+                        )
+                      )],
                   ),
-                  const SizedBox(height: 5),
-
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
